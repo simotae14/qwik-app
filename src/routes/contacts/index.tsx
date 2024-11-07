db8517c2f781b0aba902b6633a645348239b4340
Simone Taeggi <startae14@gmail.com>
2024-11-07 11:11:55 +0100

    UI Component interactivity

--- src/routes/contacts/index.tsx
+++ src/routes/contacts/index.tsx
@@ -1,4 +1,4 @@
-import { component$, Resource, useStylesScoped$ } from "@builder.io/qwik";
+import { component$, Resource, useSignal, useStylesScoped$ } from "@builder.io/qwik";
 import { useEndpoint, type RequestHandler } from "@builder.io/qwik-city";
 import { type Contact, CONTACTS } from "./fake-db";
 import CSS from './index.css?inline';
@@ -13,16 +13,20 @@ export const onGet: RequestHandler<Contact[]> = async () => {
 export default component$(() => {
   useStylesScoped$(CSS);
   const endpoint = useEndpoint<typeof onGet>();
+  const filter = useSignal('');
   return (
     <div>
       <h1>Contacts</h1>
+      <input placeholder="Search" onInput$={(event) => {
+        filter.value = (event.target as HTMLInputElement).value;
+      }} />
       <Resource
         value={endpoint}
         onPending={() => <div>Loading...</div>}
         onResolved={(contacts) => {
           return (
             <ul>
-              {contacts.map((contact) => (
+              {contacts.filter((cont) => cont.name.toLowerCase().indexOf(filter.value.toLowerCase()) > -1).map((contact) => (
                 <li>
                   <img src={contact.avatar} alt={contact.name} />
                   {contact.name}
